4b36dd16f0e62abbbc4a1085a15becca52963b5b
xc <1907402902@qq.com>
2022-04-04 17:57:44 +0800

    init

---
+++ demo-log-server/src/test/java/com/example/demologserver/DemoLogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demologserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
